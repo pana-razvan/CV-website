ce1494230ad01f2e23d1fbd1ee0c7c2e8fefb58d
razvan.pana <razvan.pana>
2022-04-29 08:29:20 +0300

    initial commit2

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask()
+
+
+@app.route("/home", methods="GET")
+def home():
+    return render_template('index.html')
