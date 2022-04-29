d86066490e58f4a14b2e70a638ec1e6138b376c1
razvan.pana <razvan.pana>
2022-04-29 10:51:37 +0300

    update

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
-import requests
 
-app = Flask()
+app = Flask(__name__)
+Bootstrap(app)
 
 
-@app.route("/home", methods="GET")
+@app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
